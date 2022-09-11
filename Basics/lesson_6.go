d440eb3ec341e4e3584321c883fe3f2cc7ddefe0
cute-wei-ting <a0919111125@gmail.com>
2022-09-11 16:17:41 +0800

    Feat: Added lesson6

---
+++ Basics/lesson_6.go
@@ -0,0 +1,5 @@
+/* Concurrency */
+// https://go.dev/tour/concurrency/1
+//
+// Exercise: Equivalent Binary Trees :https://gist.github.com/kaipakartik/8120855
+// Exercise: Web Crawler :https://go.dev/tour/concurrency/10
